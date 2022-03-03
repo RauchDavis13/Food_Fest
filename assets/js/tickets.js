66b10abb4345724772c21b1f7efdaca8df6908ca
KRD13 <rauchdavis13@gmail.com>
2022-03-03 07:33:05 -0500

    code snapshot 19.3

--- assets/js/tickets.js
+++ assets/js/tickets.js
@@ -1,7 +1,13 @@
-require("bootstrap");
-const createEl = require("./domMethods");
+require('bootstrap');
 
-$(document).ready( function() {
+if (window.location.href.indexOf("tickets") > -1) {
+
+  const purchaseBtn = document.getElementById("purchaseBtn");
+  const purchaseEmail = document.getElementById("purchaseEmail");
+  const modalEl = document.querySelector(".modal-content");
+  const modalBodyEl = document.querySelector(".modal-body");
+  const modalFooterEl = document.querySelector(".modal-footer");
+
 
   function purchaseTicket () {
 
@@ -16,4 +22,4 @@ $(document).ready( function() {
     
   }
   purchaseBtn.addEventListener("click", purchaseTicket);
-})
+}
